2b1108d752597bfa919fb70b2eacd1f3432f4270
mohamed ismail <kiyas9148@gmail.com>
2026-09-14 19:07:39 +0530

    Fix missing variable declaration for payload

--- tourism-service/src/test/java/com/ampara/tourism/controller/TouristPlaceControllerTest.java
+++ tourism-service/src/test/java/com/ampara/tourism/controller/TouristPlaceControllerTest.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-
+import org.springframework.test.context.ActiveProfiles;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
@@ -18,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(TouristPlaceController.class)
 @AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles("test")
 class TouristPlaceControllerTest {
 
     @Autowired
--- tourism-service/src/test/java/com/ampara/tourism/security/JwtUtilTest.java
+++ tourism-service/src/test/java/com/ampara/tourism/security/JwtUtilTest.java
@@ -5,10 +5,11 @@ import com.ampara.tourism.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.test.context.ActiveProfiles;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class JwtUtilTest {
 
     @Autowired
