94ab663b221936d0de500e39fa0f5871772f2840
mohamed ismail <kiyas9148@gmail.com>
2026-09-15 12:58:51 +0530

    Fix import order for ActiveProfiles

--- tourism-service/src/test/java/com/ampara/tourism/controller/AttractionControllerTest.java
+++ tourism-service/src/test/java/com/ampara/tourism/controller/AttractionControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,6 +16,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @WebMvcTest(AttractionController.class)
 @AutoConfigureMockMvc(addFilters = false)
